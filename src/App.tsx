f512dc9d51173564c2b8760ac488b484854e3b65
manulangat <emmanuelthedeveloper@gmail.com>
2022-09-25 11:23:30 +0300

    chore: Add blog, github links and fix linkeidn

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import React , {Fragment} from 'react';
+import React, { Fragment } from "react";
 
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-// import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+// import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import { Helmet } from "react-helmet";
-import Skills from './components/Skills/Skills';
-
-
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <Fragment>
       <Helmet>
-        ‍<title>Emmanuel Kipchirchir langat, Langat Kipchirchir Emmanuel , Kipchirchir Langat Emmanuel, manulangat1, Farmer</title>‍
+        ‍
+        <title>
+          Emmanuel Kipchirchir langat, Langat Kipchirchir Emmanuel , Kipchirchir
+          Langat Emmanuel, manulangat1, Farmer
+        </title>
+        ‍
         <meta
           name="description"
-          content="Software engineeer, programmer, web developer , machine learning engineer, Emmanuel Kipchirchir Langat"
+          content="Software engineeer, programmer, web developer , machine learning engineer, Emmanuel Kipchirchir Langat, devsecops engineer, jenkins, AWS, Terrarom and kubbernetties expert"
         />
       </Helmet>
       <Navbar />
-      <section style={{ height:'100vh', width:'100%'}}>
+      <section style={{ height: "100vh", width: "100%" }}>
         <Home />
         {/* <Projects /> */}
         <Skills />
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,56 +1,77 @@
-import { Container, Typography, List, ListItem, ListItemButton,  ListItemIcon } from "@mui/material";
+import {
+  Container,
+  Typography,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+} from "@mui/material";
 import React, { Fragment } from "react";
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => {
   const onClick = (link: string) => window.open(link, "__blank");
-    const iconList = [
-        {
-            id:1,
-            link:'https://github.com/manulangat1',
-            name:'Github',
-            icon: <GitHubIcon />
-        }, 
-        {
-            id:2,
-            link:'https://www.linkedin.com/in/emmanuel-langat-7b547a158/',
-            name:'Linkdin',
-            icon: <LinkedInIcon />
-        }, 
-        {
-            id:3,
-            link:'https://twitter.com/langatmanuK',
-            name:'Twitter',
-            icon: <TwitterIcon />
-        }, 
-    ]
+  const iconList = [
+    {
+      id: 1,
+      link: "https://github.com/manulangat1",
+      name: "Github",
+      icon: <GitHubIcon />,
+    },
+    {
+      id: 2,
+      link: "https://www.linkedin.com/in/emmanuel-langat/",
+      name: "Linkdin",
+      icon: <LinkedInIcon />,
+    },
+    {
+      id: 3,
+      link: "https://twitter.com/langatmanuK",
+      name: "Twitter",
+      icon: <TwitterIcon />,
+    },
+  ];
 
   return (
     <Fragment>
       <footer
-        style={{ textAlign: "center", padding: "3rem", background: "#2f4454" , color:'#ffffff'}}
+        style={{
+          textAlign: "center",
+          padding: "3rem",
+          background: "#2f4454",
+          color: "#ffffff",
+        }}
       >
         <Container maxWidth="xl">
           <Typography variant="h6" gutterBottom>
             Kipchirchir Langat
           </Typography>
           <p>{new Date().getFullYear()}</p>
-          <List style={{ display:'flex', flexDirection:'row', justifyContent:'center', justifySelf:'center', textAlign:'center', alignItems:'center', width:'50%', margin:'auto'}} >
-          {
-            iconList.map((icon) => (
+          <List
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              justifySelf: "center",
+              textAlign: "center",
+              alignItems: "center",
+              width: "50%",
+              margin: "auto",
+            }}
+          >
+            {iconList.map((icon) => (
               <ListItem disableGutters key={icon.id} disablePadding>
-                    <ListItemButton disableGutters onClick={() => onClick(icon.link)}>
-                      <ListItemIcon >{icon.icon}</ListItemIcon>
-                      {/* <ListItemText  >{icon.name}</ListItemText> */}
-                    </ListItemButton>
-                  </ListItem>
-            ))
-          }
+                <ListItemButton
+                  disableGutters
+                  onClick={() => onClick(icon.link)}
+                >
+                  <ListItemIcon>{icon.icon}</ListItemIcon>
+                </ListItemButton>
+              </ListItem>
+            ))}
           </List>
-          
         </Container>
       </footer>
     </Fragment>
