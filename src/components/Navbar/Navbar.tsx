98cd367b81fd2af667827c2b4f1d989e7e64d649
manulangat <emmanuelthedeveloper@gmail.com>
2022-09-25 11:31:14 +0300

    chore: remove unused files

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,53 +1,53 @@
 import {
   AppBar,
-  Box,
+  // Box,
   Container,
-  IconButton,
-  Menu,
-  MenuItem,
-  MenuList,
+  // IconButton,
+  // Menu,
+  // MenuItem,
+  // MenuList,
   Toolbar,
   Typography,
 } from "@mui/material";
 import React, { Fragment } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
+// import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
+  // const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+  //   null
+  // );
+  // const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+  //   null
+  // );
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+  // const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorElNav(event.currentTarget);
+  // };
+  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+  //   setAnchorElUser(event.currentTarget);
+  // };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  // const handleCloseNavMenu = () => {
+  //   setAnchorElNav(null);
+  // };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  const pages = [
-    {
-      id: 1,
-      page: "Home",
-    },
-    {
-      id: 2,
-      page: "Home",
-    },
-    {
-      id: 3,
-      page: "Home",
-    },
-  ];
+  // const handleCloseUserMenu = () => {
+  //   setAnchorElUser(null);
+  // };
+  // const pages = [
+  //   {
+  //     id: 1,
+  //     page: "Home",
+  //   },
+  //   {
+  //     id: 2,
+  //     page: "Home",
+  //   },
+  //   {
+  //     id: 3,
+  //     page: "Home",
+  //   },
+  // ];
   return (
     <Fragment>
       <AppBar position="static" style={{ background: "#254e58" }}>
@@ -81,23 +81,15 @@ const Navbar = () => {
               </Typography>
             </div>
 
-            {pages.map((page) => (
-              <MenuItem
-                key={page.id}
-                sx={{
-                  display: { xs: "none", sm: "none", md: "none" },
-                }}
-              >
-                <Typography
-                  sx={{
-                    display: { xs: "none", sm: "none", md: "none" },
-                  }}
-                  textAlign="center"
-                >
-                  {page.page}
-                </Typography>
+            {/* {pages.map((page) => (
+              <MenuItem key={page.id} sx={{
+                display: { xs: "none", sm: "none", md: "none" },
+              }} >
+                <Typography sx={{
+                        display: { xs: "none", sm: "none", md: "none" },
+                      }} textAlign="center">{page.page}</Typography>
               </MenuItem>
-            ))}
+            ))} */}
           </Toolbar>
         </Container>
       </AppBar>
